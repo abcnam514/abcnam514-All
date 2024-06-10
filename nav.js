cfaa54208b90ddf64ffba19ad3ffbf6317e0e0b6
abcnam514 <147784898+abcnam514@users.noreply.github.com>
2024-06-11 00:21:02 +0900

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,5 +1,15 @@
 // 메뉴 항목 선택
 const menuItems = document.querySelectorAll('.nav_menu li');
+const logo = document.querySelector('#logo');
+
+
+logo.addEventListener('click', () => {
+  const mainSection = document.getElementById('main');
+  if (mainSection) {
+    mainSection.scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
 
 // 각 메뉴 항목에 클릭 이벤트 리스너 추가
 menuItems.forEach((item, index) => {
